dffabf821db4105744ab675cfef9478993f8ffd3
Ashish Pandey <ap965599@gmail.com>
2026-09-01 18:37:55 +0530

    feat: stream camera frames over USB and Wi-Fi

--- firmware/esp32/src/main.cpp
+++ firmware/esp32/src/main.cpp
@@ -1,17 +1,48 @@
 #include <Arduino.h>
 
 #include "camera.h"
+#include "system_config.h"
+#include "usb_transport.h"
+#include "wifi_transport.h"
+
+namespace {
+
+UsbTransport usbTransport;
+WifiTransport wifiTransport;
+
+} // namespace
 
 void setup()
 {
+    usbTransport.begin();
+    delay(500);
+
     if (!camera.begin()) {
+        Serial.println("[SYSTEM][FATAL] MLX90640 initialization failed.");
         while (true) {
             delay(1000);
         }
+    }
+
+    if (wifiTransport.begin()) {
+        Serial.print("[WIFI] SSID: ");
+        Serial.println(ThermalConfig::WIFI_AP_SSID);
+        Serial.print("[WIFI] IP: ");
+        Serial.println(WiFi.softAPIP());
+        Serial.print("[WIFI] TCP port: ");
+        Serial.println(ThermalConfig::WIFI_PORT);
+    } else {
+        Serial.println("[WIFI][WARN] Access point startup failed.");
     }
 }
 
 void loop()
 {
-    camera.update();
+    if (camera.update()) {
+        const ThermalFrame &frame = camera.getFrame();
+        usbTransport.sendFrame(frame);
+        wifiTransport.sendFrame(frame);
+    }
+
+    delay(10);
 }
