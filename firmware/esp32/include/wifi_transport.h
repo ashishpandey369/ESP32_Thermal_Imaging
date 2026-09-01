c07e6a6922c68f8e336e691132c3657ae4ec5055
Ashish Pandey <ap965599@gmail.com>
2026-09-01 18:37:31 +0530

    feat: add Wi-Fi thermal transport interface

---
+++ firmware/esp32/include/wifi_transport.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include "thermal_transport.h"
+
+class WifiTransport final : public ThermalTransport
+{
+public:
+    bool begin() override;
+    void sendFrame(const ThermalFrame &frame) override;
+
+private:
+    WiFiServer server{ThermalConfig::WIFI_PORT};
+    WiFiClient client;
+};
