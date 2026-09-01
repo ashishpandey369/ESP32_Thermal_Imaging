70d239c2b3cfae2c98ed3603f7842c9610af2f24
Ashish Pandey <ap965599@gmail.com>
2026-09-01 18:26:38 +0530

    refactor: move system config header into include

---
+++ firmware/esp32/include/system_config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Arduino.h>
+
+namespace ThermalConfig {
+
+constexpr uint32_t SERIAL_BAUD = 115200;
+constexpr uint32_t I2C_FREQUENCY = 400000;
+constexpr uint8_t MLX90640_REFRESH_RATE_CODE = 0x03; // 2 Hz during initial bring-up
+constexpr float EMISSIVITY = 0.95f;
+constexpr float TA_SHIFT = 8.0f;
+constexpr size_t THERMAL_PIXEL_COUNT = 32 * 24;
+
+} // namespace ThermalConfig
