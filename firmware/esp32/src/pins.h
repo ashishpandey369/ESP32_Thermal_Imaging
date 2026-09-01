e14a7fcf5f80c649a66fc9db77e3a37ec3afcff5
Ashish Pandey <ap965599@gmail.com>
2026-09-01 18:19:52 +0530

    feat: add centralized ESP32 hardware pin definitions

---
+++ firmware/esp32/src/pins.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <Arduino.h>
+
+// MLX90640 I2C connection
+constexpr uint8_t CAMERA_SDA_PIN = 21;
+constexpr uint8_t CAMERA_SCL_PIN = 22;
+constexpr uint8_t CAMERA_I2C_ADDRESS = 0x33;
