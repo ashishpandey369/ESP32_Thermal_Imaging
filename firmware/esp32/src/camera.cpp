3adc1ed481e86b64eaa3af66154e3b0149459aa7
Ashish Pandey <ap965599@gmail.com>
2026-09-02 18:13:41 +0530

    fix: migrate camera driver to Adafruit MLX90640 API

--- firmware/esp32/src/camera.cpp
+++ firmware/esp32/src/camera.cpp
@@ -1,15 +1,14 @@
 #include "camera.h"
 #include "pins.h"
+#include "system_config.h"
 
+#include <Adafruit_MLX90640.h>
 #include <Wire.h>
-#include <math.h>
-
-#include "MLX90640_API.h"
-#include "MLX90640_I2C_Driver.h"
+#include <cmath>
 
 namespace {
 
-paramsMLX90640 mlx90640;
+Adafruit_MLX90640 mlx90640;
 
 } // namespace
 
@@ -27,22 +26,15 @@ bool ThermalCamera::initializeSensor()
         return false;
     }
 
-    uint16_t eeData[832];
-    int status = MLX90640_DumpEE(CAMERA_I2C_ADDRESS, eeData);
-    if (status != 0) {
+    if (!mlx90640.begin(CAMERA_I2C_ADDRESS, &Wire)) {
         return false;
     }
 
-    status = MLX90640_ExtractParameters(eeData, &mlx90640);
-    if (status != 0) {
-        return false;
-    }
+    mlx90640.setMode(MLX90640_CHESS);
+    mlx90640.setResolution(MLX90640_ADC_18BIT);
+    mlx90640.setRefreshRate(MLX90640_4_HZ);
 
-    status = MLX90640_SetRefreshRate(
-        CAMERA_I2C_ADDRESS,
-        ThermalConfig::MLX90640_REFRESH_RATE_CODE);
-
-    return status == 0;
+    return true;
 }
 
 bool ThermalCamera::begin()
@@ -56,37 +48,23 @@ bool ThermalCamera::begin()
 
 bool ThermalCamera::readThermalFrame()
 {
-    uint16_t frameData[834];
-
-    float minimum = 1000.0f;
-    float maximum = -1000.0f;
-    float sum = 0.0f;
-
-    for (uint8_t subpage = 0; subpage < 2; ++subpage) {
-        const int status = MLX90640_GetFrameData(CAMERA_I2C_ADDRESS, frameData);
-        if (status < 0) {
-            return false;
-        }
-
-        const float ambientTemperature = MLX90640_GetTa(frameData, &mlx90640);
-        const float reflectedTemperature = ambientTemperature - ThermalConfig::TA_SHIFT;
-
-        MLX90640_CalculateTo(
-            frameData,
-            &mlx90640,
-            ThermalConfig::EMISSIVITY,
-            reflectedTemperature,
-            temperatures);
+    if (mlx90640.getFrame(temperatures) != 0) {
+        return false;
     }
 
+    float minimum = INFINITY;
+    float maximum = -INFINITY;
+    float sum = 0.0f;
+
     for (std::size_t i = 0; i < ThermalConfig::THERMAL_PIXEL_COUNT; ++i) {
-        if (!isfinite(temperatures[i])) {
+        if (!std::isfinite(temperatures[i])) {
             return false;
         }
 
         minimum = min(minimum, temperatures[i]);
         maximum = max(maximum, temperatures[i]);
         sum += temperatures[i];
+        frame.pixels[i] = temperatures[i];
     }
 
     frame.frameNumber++;
@@ -95,10 +73,6 @@ bool ThermalCamera::readThermalFrame()
     frame.maximum = maximum;
     frame.average = sum / static_cast<float>(ThermalConfig::THERMAL_PIXEL_COUNT);
     frame.center = temperatures[(12 * 32) + 16];
-
-    for (std::size_t i = 0; i < ThermalConfig::THERMAL_PIXEL_COUNT; ++i) {
-        frame.pixels[i] = temperatures[i];
-    }
 
     return true;
 }
